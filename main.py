2f17e19279f8f58d4d5895b1bd0ac3481788d45b
heserral <heserral@gmail.com>
2024-07-30 21:42:39 +0200

    encrypt ofertas ok, solucionado problema nginx max file size upload y multi-form data

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://evalia.inovalabs.es","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
